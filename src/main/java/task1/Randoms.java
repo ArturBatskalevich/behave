25ec73befc1fd7ec1dae03330a2e7dc60c6c0326
Artur Batskalevich <artur.batskalevich@gmail.com>
2022-01-18 21:06:43 +0300

    First commit

---
+++ src/main/java/task1/Randoms.java
@@ -0,0 +1,29 @@
+package task1;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer>{
+
+    protected Random random;
+
+    public Randoms(int min,int max){
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
+    }
+}

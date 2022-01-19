eba2767878f3b7cbae597b15b503bb7c71c4ecf5
Artur Batskalevich <artur.batskalevich@gmail.com>
2022-01-19 16:53:53 +0300

    Correct Randoms

--- src/main/java/task1/Randoms.java
+++ src/main/java/task1/Randoms.java
@@ -1,43 +1,36 @@
 package task1;
 
-import java.util.Iterator;
-import java.util.Random;
+import java.util.*;
 
 public class Randoms implements Iterable<Integer>{
 
     protected Random random;
 
-    private final Integer[] arrayList;
-    private final int currentSize;
+    private List<Integer> list;
 
     public Randoms(int min,int max){
-        final int count = 100; // количество сгенерированных случайных чисел
-        int i = 0;
-        int diff = max - min;
-        this.arrayList = new Integer[count + 1];
+        list = new ArrayList<>();
         random = new Random();
-        while (i <= count){
-            arrayList[i] = random.nextInt(diff + 1) + min;
-            i++;
-        }
-        this.currentSize = arrayList.length;
-    }
 
+        while (max > min) {
+            list.add(max);
+            max--;
+        }
+
+    }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
 
-            private int currentIndex = 0;
-
             @Override
             public boolean hasNext() {
-                return currentIndex < currentSize && arrayList[currentIndex] != null;
+                return true;
             }
 
             @Override
             public Integer next() {
-                return arrayList[currentIndex++];
+                return list.get(random.nextInt(list.size()));
             }
 
             @Override
